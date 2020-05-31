93161f3a60f2ea67b0c64a3325dab8c32e81d0d4
Duvan Rosero <duvanrosero1995@gmail.com>
2020-05-31 08:53:07 -0500

    Selectores en jquery

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    //TODO
+    console.log("Documento cargado...");
+
+    // document.querySelector('h1').innerHTML = "Hola mundo";
+
+    // var h1 = $('h1');
+    // h1.text("Hola mundo desde JQuery");
+    // h1.html('<s>Hola mundo</s>');
+
+    $('.display-3').text('Usando selectores con clase');
+    $('#parrafo').text("Este es el parrafo...");
+
+
+    $('.container h1').addClass('text-info');
+
+
+
+
+});
